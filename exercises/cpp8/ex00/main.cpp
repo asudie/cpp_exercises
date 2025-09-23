751eab8c206d2321c65e38b30f5196a44640cbb7
Aleksandra Smolniakova <smolnyakovaae@gmail.com>
2025-09-23 18:11:07 +0000

    finished ex00

--- exercises/cpp8/ex00/main.cpp
+++ exercises/cpp8/ex00/main.cpp
@@ -5,20 +5,17 @@ int main(){
     int arr[] = {1, 2, 3, 4, 5};
      std::vector<int> v(arr, arr + 5);
     try{
-        easyfind(v, 5)
+        easyfind(v, 5);
     }
     catch(std::exception &e){
-        std::cout << what.e
+        std::cout << e.what() << std::endl;
     }
-    if(!easyfind(v, 5))
-        std::cout << "Found it!" << std::endl;
-    else
-        std::cout << "Couldn't find it!" << std::endl;
-
-    if(!easyfind(v, 7))
-        std::cout << "Found it!" << std::endl;
-    else
-        std::cout << "Couldn't find it!" << std::endl;
+    try{
+        easyfind(v, 7);
+    }
+    catch(std::exception &e){
+        std::cout << e.what() << std::endl;
+    }
     
     return 0;
 }
