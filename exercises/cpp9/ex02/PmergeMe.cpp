1df014e9f95a3c56d801816115587bb7582625a2
asudie <smolnyakovaae@gmail.com>
2026-02-04 19:20:10 +0100

    formed main and pend Now sort main and insert pend

--- exercises/cpp9/ex02/PmergeMe.cpp
+++ exercises/cpp9/ex02/PmergeMe.cpp
@@ -1,16 +1,50 @@
 #include "PmergeMe.hpp"
 
-template <template<typename T, typename ...> class Container>
+template <template<typename, typename> class Container>
 void PmergeMe<Container>::addElmnt(std::pair<unsigned int, unsigned int> pair){
     _container.push_back(pair);
 }
 
-template <template<typename T, typename ...> class Container>
+template <template<typename, typename> class Container>
 void PmergeMe<Container>::printContainer(){
-    std::Container<pair_t>::iterator it = data_.begin();
-    for(; it != data_.end(); it++)
+    //std::cout << "DEBUG[PRINT1]" << std::endl;
+    typename Container<pair_t, std::allocator<pair_t> >::iterator it = _container.begin();
+    std::cout << "DEBUG[PRINT LOOP]" << std::endl;
+    for(; it != _container.end(); it++){
         std::cout << "1[" << it->first << "]" << "2[" << it->second << "]" << std::endl;
+    }
+    if(_left > 0)
+        std::cout << "Left[" << _left << "]" << std::endl;
+
 }
+
+template <template<typename, typename> class Container>
+void PmergeMe<Container>::printChains(){
+    typename Container<unsigned int, std::allocator<unsigned int> >::iterator it = _main.begin();
+    std::cout << "MAIN" << std::endl;
+    for(; it != _main.end(); it++)
+       std::cout << *it << std::endl;
+    std::cout << "PEND" << std::endl;
+    it = _pend.begin();
+    for(; it != _pend.end(); it++)
+       std::cout << *it << std::endl;
+}
+
+template <template<typename, typename> class Container>
+void PmergeMe<Container>::formChains(){
+    typename Container<pair_t, std::allocator<pair_t> >::iterator it = _container.begin();
+    for(; it != _container.end(); it++){
+        _pend.push_back(it->first);
+        _main.push_back(it->second);
+    }
+    printChains();
+}
+
+template <template<typename, typename> class Container>
+void PmergeMe<Container>::setLeft(int n){_left = n;}
+
+template <template<typename, typename> class Container>
+int PmergeMe<Container>::getLeft(){return _left;}
 
 /*void PmergeVector::addElmnt(std::pair<unsigned int, unsigned int> pair){
     _vector.push_back(pair);
@@ -21,36 +55,57 @@ void PmergeDeque::addElmnt(std::pair<unsigned int, unsigned int> pair){
 }*/
 
 void PmergeVector::FordJohnson(char **argv){
-    while(argv){
-        // add check if it's the last element
-        unsigned int n1 = std::stoi(*argv);
+    std::cout << "VECTOR" << std::endl;
+    setLeft(-42);
+    argv++;
+    while(*argv){
+        unsigned int n1 = static_cast<unsigned int>(std::stoi(*argv));
         argv++;
-        unsigned int n2 = std::stoi(*argv);
-        std::cout << "n1 = " << n1 << "n2 = " << n2 << std::endl;
+        if(!*argv){
+            setLeft(n1);
+            break;
+        }
+        unsigned int n2 = static_cast<unsigned int>(std::stoi(*argv));
+        std::cout << "n1 = " << n1 << ", n2 = " << n2 << std::endl;
         std::pair<unsigned int, unsigned int> pair = formPair(n1, n2);
         addElmnt(pair);
         argv++;
     }
     printContainer();
+    formChains();
 }
+
+
 void PmergeDeque::FordJohnson(char **argv){
-    while(argv){
-        // add check if it's the last element
-        unsigned int n1 = std::stoi(*argv);
+    std::cout << "VECTOR" << std::endl;
+    setLeft(-42);
+    argv++;
+    while(*argv){
+        unsigned int n1 = static_cast<unsigned int>(std::stoi(*argv));
         argv++;
-        unsigned int n2 = std::stoi(*argv);
-        std::cout << "n1 = " << n1 << "n2 = " << n2 << std::endl;
+        if(!*argv){
+            setLeft(n1);
+            break;
+        }
+        unsigned int n2 = static_cast<unsigned int>(std::stoi(*argv));
+        std::cout << "n1 = " << n1 << ", n2 = " << n2 << std::endl;
         std::pair<unsigned int, unsigned int> pair = formPair(n1, n2);
         addElmnt(pair);
         argv++;
     }
+    printContainer();
+    formChains();
+    // sort _main
 }
 
 std::pair<unsigned int, unsigned int> formPair(unsigned int n1, unsigned int n2){
     std::pair<unsigned int, unsigned int> pair;
-    if(n1 < n2)
-        pair = {n1, n2};
-    else
-        pair = {n2, n1};
+    if(n1 < n2){
+        pair.first = n1;
+        pair.second = n2;
+    }else{
+        pair.first = n2;
+        pair.second = n1;
+    }
     return pair;
 }
--- exercises/cpp9/ex02/PmergeMe.hpp
+++ exercises/cpp9/ex02/PmergeMe.hpp
@@ -4,14 +4,15 @@
 #include <deque>
 #include <iostream>
 #include <string>
+#include <memory>
 
-template <template<typename T, typename ...> class Container>
+template <template<typename, typename> class Container>
 class PmergeMe{
 private:
-    using pair_t = std::pair<unsigned int, unsigned int>;
-    Container<pair_t> _container;
-    std::vector<unsigned int> _main;
-    std::vector<unsigned int> _pend;
+    typedef std::pair<unsigned int, unsigned int> pair_t;
+    Container<pair_t, std::allocator<pair_t> > _container;
+    Container<unsigned int, std::allocator<unsigned int> > _main;
+    Container<unsigned int, std::allocator<unsigned int> > _pend;
     int _left;
     // timer
 public:
@@ -19,8 +20,11 @@ public:
     //void FordJohnson(char **argv);
     void parse();
     void addElmnt(std::pair<unsigned int, unsigned int> pair);
-    // void setLeft(int n);
+    void setLeft(int n);
+    int getLeft();
     void printContainer();
+    void formChains();
+    void printChains();
 };
 
 class PmergeVector : public PmergeMe<std::vector> {
@@ -42,7 +46,7 @@ private:
     /*std::deque<std::pair<unsigned int, unsigned int>> _deque;
     std::deque<unsigned int> _main;
     std::deque<unsigned int> _pend;*/
-    int _left;
+    //int _left;
     // timer
 public:
     //PmergeDeque(){start timer}
