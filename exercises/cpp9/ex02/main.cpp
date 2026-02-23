f7a53fdeb53b1484910e6ac1fa96cc179998695c
Aleksandra Smolniakova <smolnyakovaae@gmail.com>
2026-02-23 16:12:59 +0100

    Update argument check and add printArgv function

--- exercises/cpp9/ex02/main.cpp
+++ exercises/cpp9/ex02/main.cpp
@@ -1,12 +1,29 @@
 #include "PmergeMe.hpp"
 
+void printArgv(char **argv){
+    std::cout << "Before: ";
+    for(int i = 1; argv[i]; i++){
+        std::cout << argv[i]   << " ";
+        if(i == 10)
+        {
+            std::cout << "[...] ";
+            break;
+        }
+    } 
+    std::cout << std::endl;
+}
+
 void my_algo(PmergeDeque *deque, PmergeVector *vector, char **argv){
+    printArgv(argv);
     deque->FordJohnson(argv);
     vector->FordJohnson(argv);
+    //deque->finalPrint();
+    deque->timerPrint();
+    vector->timerPrint();
 }
 
 int main(int argc, char **argv){
-    if(argc > 1) {
+    if(argc > 2) {
         PmergeDeque deque;
         PmergeVector vector;
         my_algo(&deque, &vector, argv);
