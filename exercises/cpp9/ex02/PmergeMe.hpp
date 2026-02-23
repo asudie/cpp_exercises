bdcfd044cd8f187a283151a1ea5d36e10fb6949e
Aleksandra Smolniakova <smolnyakovaae@gmail.com>
2026-02-23 16:12:15 +0100

    Replace chrono with ctime and add algorithm

--- exercises/cpp9/ex02/PmergeMe.hpp
+++ exercises/cpp9/ex02/PmergeMe.hpp
@@ -6,7 +6,9 @@
 #include <iostream>
 #include <string>
 #include <memory>
-#include <chrono>
+#include <ctime>
+#include <cstdlib>
+#include <algorithm>
 
 template <template<typename, typename> class Container>
 class PmergeMe{
@@ -16,8 +18,8 @@ protected:
     Container<unsigned int, std::allocator<unsigned int> > _main;
     Container<unsigned int, std::allocator<unsigned int> > _pend;
     int _left;
-    std::chrono::time_point<std::chrono::high_resolution_clock> _start;
-    std::chrono::time_point<std::chrono::high_resolution_clock> _end;
+    clock_t _start;
+    clock_t _end;
     std::string _name;
     int _size;
 public:
@@ -37,13 +39,20 @@ public:
     void insertJacobs();
     std::deque<int> genJacobs();
     int findSmallstCont(int n);
-    void recursiveSortCont(int n);
+    // void recursiveSortCont(int n);
+    //void recursiveSortCont(Container<pair_t, std::allocator<pair_t> >& cont);
     void putElm(int n, int index);
     void finalPrint();
+    void timerPrint();
     void parseArgs(char **argv);
     void startTimer();
     void stopTimer();
+    void genFordJonson(Container<pair_t, std::allocator<pair_t> >& cont);
     virtual void pushFront(unsigned int n) = 0;
+
+private:
+    void insertPendIntoWinners(Container<pair_t, std::allocator<pair_t> >& winners, Container<pair_t, std::allocator<pair_t> >& pend);
+    std::deque<int> genJacobs(int size);
 };
 
 class PmergeVector : public PmergeMe<std::vector> {
